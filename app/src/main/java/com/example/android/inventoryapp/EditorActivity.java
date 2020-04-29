076cdebf86646a366ef29a16fe80254ae9eef2a5
Credo-Mbikayi <credo.mbikayi@umontreal.ca>
2020-04-29 18:41:53 -0400

    Remove unecessary libraries

--- app/src/main/java/com/example/android/inventoryapp/EditorActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/EditorActivity.java
@@ -25,15 +25,12 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NavUtils;
 
-import com.google.android.material.button.MaterialButton;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.textfield.TextInputEditText;
 import com.example.android.inventoryapp.data.InventoryContract.InventoryEntry;
-import com.google.android.material.textfield.TextInputLayout;
 
 
 import java.io.InputStream;
-import java.text.NumberFormat;
 
 public class EditorActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
